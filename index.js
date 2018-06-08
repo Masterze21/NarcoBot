a0c6159bcf77348d29d83605d6fcc93575d7e3b8
Masterze21 <39980114+Masterze21@users.noreply.github.com>
2018-06-08 09:45:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,99 +10,100 @@ client.login(process.env.TOKEN);
 
 client.on("ready", () => {
     console.log("Bot on")
-    client.user.setGame(" par Masterze 21#3970 (m!aide)");
+    var nombre = client.guilds.size
+client.user.setGame(`Serveurs : ${nombre}  | m!help`);
 });
 
 client.on('message', message => {
 
     if(message.content.startsWith(prefix + "ct")){
-        message.reply("Bot crée par Masterze 21#3970");
+        message.reply("Pour me contacter : masterzey06@gmail.com ou Masterze 21#0001 ");
         console.log ('bot ct');
     }
 
-    if(message.content === prefix + "aide"){
+    if(message.content === prefix + "help"){
         var help_embed = new Discord.RichEmbed()
         .setColor("#FF6600")
-        .setTitle("m!ct")
-        .setDescription("Essaye tu vera ;)")
-        .addField("m!aide", "Affiche les commands.")
-        .addField("m!stat", "Afiche tes statistiques Discord !")
-        .addField("m!Info", "Affiche le nombre de membre, de catégories et de salons !")
-        .addField("m!kick", "m!kick @user")
-        .addField("m!ban", "m!ban @user")
-        .addField("m!clear", "supprimer les message (1-100)")
-        .setFooter("Menu des - Commandes")
+        .setTitle(":arrow_down_small: Voici les commandes disponible :arrow_down_small: ")
+        .setDescription("Je suis un bot de Modérations :tools: et Utilitaires :gear: pour l'intant)")
+        .addField("m!ct", "Pour me contacter:e_mail: ")
+        .addField("m!help", "Affiche les commandes:hammer_pick: .")
+        .addField("m!profil", "Affiche tes statistiques Discord:chart_with_upwards_trend: !")
+        .addField("m!info", "Affiche les info Serveur:satellite: ")
+        .addField("m!kick", "Kick la personne mentionné:door:")
+        .addField("m!ban", "Ban la personne mentionné:hammer:")
+        .addField("m!clear", "supprimer les message (:one:-:one::zero::zero:)")
+        .addField("m!ping", "Temp de latence avec le serveur:ping_pong:")
+        .setFooter("Par Masterze 21#0001")
         message.channel.sendMessage(help_embed);
-        console.log ("commandes aide activer")
+        console.log ("commandes aide")
     }
 
-    if(message.content === prefix + "Info") {
+    if(message.content === prefix + "info") {
         var info_embed = new Discord.RichEmbed()
-        .setColor("#FF6600")
+
         .setTitle("Voici les informations sur moi et le serveur !")
-        .addField(" :robot: Nom :", `${client.user.tag}`, true)
-        .addField("discriminateur du bot :hash:", `#${client.user.discriminator}`)
-        .addField(":id:", `${client.user.id}`)
-        .addField(":busts_in_silhouette:Nombres de membres.:busts_in_silhouette: ", message.guild.members.size)
-        .addField(":dividers:Nombre de catégories et de salons.:dividers:", message.guild.channels.size)
-        .setFooter("Info")
-        message.channel.sendMessage(info_embed)
-        console.log ("info commands")
+        .addField(":capital_abcd:Nom du serveur", message.guild.name, true)
+        .addField(":crown:Propriétaire", message.guild.owner, true)
+        .addField(":shield: Niveau de Vérification", message.guild.verificationLevel, true)
+        .addField(":globe_with_meridians:Région", message.guild.region, true)
+        .addField(":family:Nombres de membres.", message.guild.members.size, true)
+        .addField (":robot:Bot", message.guild.members.filter(m => m.user.bot).size, true)
+        .addField(":dividers:Nombre salons.", message.guild.channels.size, true)
+        .addField(":open_file_folder:Le nombre de rôle :", message.guild.roles.size, true)
+        .addField(":neutral_face:Nombre d'émojie", message.guild.emojis.size, true )
+        .setFooter("Par Masterze 21#0001")
+        .setColor("#FF6600")
+        message.channel.send(info_embed).catch();
+        console.log("info commands");
     }
 
     if(message.content.startsWith(prefix + "kick")) {
-        if (!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.channel.sendMessage("Tu a pas les perm x)");
-        console.log ('kick no perm');
+        if (!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.channel.send("Tu a pas la perm de faire cette commande");
        
         if(message.mentions.users.size === 0) {
-            return message.channel.sendMessage("mais mentionne le pour le kick xD");
-            console.log ('kick no mention');
+            return message.channel.send("Tu dois mentionner la personne que tu veux kick");
         }
 
         var kick = message.guild.member(message.mentions.users.first());
         if(!kick) {
-            return message.channel.sendMessage("Je ne sais pas si l'utilisateur existe :/");
-            console.log ('kick no user');
+            return message.channel.send("Je ne sais pas si l'utilisateur existe :/");;
         }
         
         if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) {
-            return message.channel.sendMessage("j'ai pas la perm pour kick :/")
-            console.log ('kick no bot perm');
+            return message.channel.send("j'ai pas la perm pour kick :/")
         }
         
        kick.kick().then(member => {
-        message.channel.sendMessage(`${member.user.username}a était kick par ${message.author.username}`)
+        message.channel.send(`${member.user.username}a était kick par ${message.author.username}`)
         console.log ('kick');
        });
     }
 
     if(message.content.startsWith(prefix + "ban")) {
         if (!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.channel.sendMessage("Tu a pas les perm x)");
-        console.log ('ban no perm');
        
         if(message.mentions.users.size === 0) {
             return message.channel.send("mais mentionne le pour le ban xD");
-            console.log ('ban no mention');
         }
 
         var ban = message.guild.member(message.mentions.users.first());
         if(!ban) {
             return message.channel.sendMessage("Je ne sais pas si l'utilisateur existe :/");
-            console.log ('ban no user');
         }
         
         if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) {
             return message.channel.send("j'ai pas la perm de ban");
-            console.log ('ban no bot perm');
+
         }
         ban.ban().then(member => {
             message.channel.sendMessage(`${member.user.username}est ban par ${member.author.username} !`)
             console.log ('ban');
         });
+
     }
     if(message.content.startsWith(prefix + "clear")){
         if(!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES")) return message.channel.sendMessage("Tu a pas la permission pour clear les message :sob: !");
-        console.log ('clear no numbers')
 
         let args = message.content.split(" ").slice(1);
 
@@ -110,29 +111,39 @@ client.on('message', message => {
         message.channel.bulkDelete(args[0]).then(() => {
             message.channel.sendMessage(`${args[0]} messages ont été supprimés !`);
             console.log ('clear')
-        })
+        }) 
     }
 
+    
+    
     if (!message.content.startsWith(prefix)) return;
-
+    
     var args = message.content.substring(prefix.length).split(" ");
-
+    
     switch (args[0].toLowerCase()) {
-        case "stat":
-
+        case "profil":
+        
         var userCreateDate = message.author.createdAt.toString().split(" ");
         var msgauthor = message.author.id;
-
+        
         var stats_embed = new Discord.RichEmbed()
-
+        
         .setColor("#FCDC12")
-        .setTitle(`Statistiques de l'utilisateur : ${message.author.username}`)
-        .addField(`ID de l'utilisateur :id:`, msgauthor, true)
-        .addField ("Date de création :", userCreateDate[1] + ' ' + userCreateDate[2] + ' ' + userCreateDate[3])
+        .setTitle(`Compte : ${message.author.username}`)
+        .addField("Pseudo", ` ${message.author.username} `)
+        .addField(":hash:", `#${message.author.discriminator}`)
+        .addField(`:id:`, msgauthor, true)
+        .addField (":date:Date de création:date: :", userCreateDate[1] + ' ' + userCreateDate[2] + ' ' + userCreateDate[3])
         .setThumbnail(message.author.avatarURL)
-        message.reply("Voici stat Discord !")
+        message.reply(":page_facing_up: Voici tes information Discord:page_facing_up:  !")
         message.channel.sendMessage({embed: stats_embed});
         console.log ('stat');
         break;
     }
+    if(message.content.startsWith(prefix + "ping"))  {
+        let ping = Math.round(client.ping);
+        console.log ('ping')
+
+        message.channel.send(':ping_pong:Pong : '+ ` ${ping } ` +  'ms`');
+        }
 });
